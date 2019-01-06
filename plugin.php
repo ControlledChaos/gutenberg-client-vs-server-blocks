a1dd40e471ed149892596cebab24b34c190f4d13
Alex Mills <Viper007Bond@users.noreply.github.com>
2019-01-05 19:13:09 -0800

    Flush out some basics and trim down the PHP.
    This time with added files.

--- plugin.php
+++ plugin.php
@@ -1,23 +1,31 @@
 <?php
 /**
- * Plugin Name: gutenberg-client-vs-server-blocks — CGB Gutenberg Block Plugin
- * Plugin URI: https://github.com/ahmadawais/create-guten-block/
- * Description: gutenberg-client-vs-server-blocks — is a Gutenberg plugin created via create-guten-block.
- * Author: mrahmadawais, maedahbatool
- * Author URI: https://AhmadAwais.com/
- * Version: 1.0.0
+ * Plugin Name: Block Test: Client vs. Server-Side
+ * Plugin URI: https://github.com/Viper007Bond/gutenberg-client-vs-server-blocks
+ * Description: Test plugin for comparing how client and server-side blocks work.
+ * Author: Alex Mills (Viper007Bond)
+ * Author URI: https://alex.blog/
  * License: GPL2+
  * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
- *
- * @package CGB
  */
 
-// Exit if accessed directly.
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
+add_action( 'enqueue_block_editor_assets', 'gutenberg_client_vs_server_blocks_cgb_editor_assets' );
 
 /**
- * Block Initializer.
+ * Enqueue Gutenberg block assets for backend editor.
+ *
+ * @uses {wp-blocks} for block type registration & related functions.
+ * @uses {wp-element} for WP Element abstraction — structure of blocks.
+ * @uses {wp-i18n} to internationalize the block's text.
+ * @uses {wp-editor} for WP editor styles.
+ * @since 1.0.0
  */
-require_once plugin_dir_path( __FILE__ ) . 'src/init.php';
+function gutenberg_client_vs_server_blocks_cgb_editor_assets() { // phpcs:ignore
+	wp_enqueue_script(
+		'gutenberg_client_vs_server_blocks-cgb-block-js', // Handle.
+		plugins_url( '/dist/blocks.build.js', dirname( __FILE__ ) ),
+		array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-editor' ),
+		filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.build.js' ),
+		true
+	);
+}
